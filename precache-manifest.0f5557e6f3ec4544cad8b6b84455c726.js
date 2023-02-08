027a4f25d194e04f35688d848e22d94830892d42
mkwiecin <mkwiecin@poczta.onet.pl>
2023-02-08 15:47:56 +0100

    Updates

---
+++ precache-manifest.0f5557e6f3ec4544cad8b6b84455c726.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591c0883f9870b82c0ea1da479070803",
+    "url": "/mario1306/starting-react.github.io/index.html"
+  },
+  {
+    "revision": "9f4530cf61b407172718",
+    "url": "/mario1306/starting-react.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e6f70607e5a1e18cde61",
+    "url": "/mario1306/starting-react.github.io/static/js/2.6522393f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/mario1306/starting-react.github.io/static/js/2.6522393f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f4530cf61b407172718",
+    "url": "/mario1306/starting-react.github.io/static/js/main.5f669375.chunk.js"
+  },
+  {
+    "revision": "e856d53fab2d633ba59c",
+    "url": "/mario1306/starting-react.github.io/static/js/runtime-main.9e87f61d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mario1306/starting-react.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mario1306/starting-react.github.io/precache-manifest.0f5557e6f3ec4544cad8b6b84455c726.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mario1306/starting-react.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
