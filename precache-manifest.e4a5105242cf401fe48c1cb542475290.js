3f5ba5be44d0b9f07203c0b64a66617f762c09d3
mkwiecin <mkwiecin@poczta.onet.pl>
2023-02-08 17:07:30 +0100

    Updates

---
+++ precache-manifest.e4a5105242cf401fe48c1cb542475290.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32063631878c12d4ebee1f2a606adb86",
+    "url": "/starting-react/index.html"
+  },
+  {
+    "revision": "bc812c7f6e7d912edb85",
+    "url": "/starting-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "df0947e1a38952fd7262",
+    "url": "/starting-react/static/js/2.b7eafc0f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starting-react/static/js/2.b7eafc0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc812c7f6e7d912edb85",
+    "url": "/starting-react/static/js/main.bee1896c.chunk.js"
+  },
+  {
+    "revision": "4d06625c17ccf1e0a0dd",
+    "url": "/starting-react/static/js/runtime-main.c0b261e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/starting-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mario1306/starting-react/precache-manifest.0d42ab87e63f3612ee7b704e787d461a.js"
+  "/starting-react/precache-manifest.e4a5105242cf401fe48c1cb542475290.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mario1306/starting-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starting-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
